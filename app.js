3641328b32047439a7a5cd25985629843dbf8fdf
ItsAlexdro28 <alejadroj5025@gmail.com>
2024-04-26 12:37:56 -0500

    feat: :sparkles: Add basic calculation
    make simple arithmethic operations

--- app.js
+++ app.js
@@ -1,21 +1,41 @@
 const btnNum = document.querySelectorAll(".btnNum");
 const btnOper = document.querySelectorAll(".btnOper");
-const ouput = document.querySelector("#output-screen");
+const output = document.querySelector("#output-screen");
 
 btnNum.forEach((btnNum) => {
     btnNum.addEventListener("click", () => {
-        var btnPress = btnNum.innerHTML;
+        let btnPress = btnNum.innerHTML;
         switch (true) {
-            case (ouput.innerHTML === "0" || ouput.innerHTML === "Error!"):
-                ouput.innerHTML = btnPress;
+            case (output.innerHTML === "0" || output.innerHTML === "Error!"):
+                output.innerHTML = btnPress;
                 break;
+            case (btnPress === "="):
+                output.innerHTML = equal(output.innerHTML);
+                break;    
             case (false):
                 break;
             default:
-                ouput.innerHTML += btnPress;
+                output.innerHTML += btnPress;
                 break;
 
             
         }
     });
 });
+
+btnOper.forEach((btnOper) => {
+    btnOper.addEventListener("click", () => {
+        let btnPress = btnOper.innerHTML;
+        switch (true) {
+            case (false):
+                break;
+            default:
+                output.innerHTML += btnPress;
+                break;
+        }
+    });
+});
+
+function equal (mathExpression) {
+    return eval((mathExpression));
+}
